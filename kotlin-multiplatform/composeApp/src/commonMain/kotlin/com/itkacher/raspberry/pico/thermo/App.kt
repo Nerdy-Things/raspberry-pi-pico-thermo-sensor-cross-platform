111f4c3bbfcede847f122784df9518c2fcbf2bd6
Eugene Tkachenko <gektor650@gmail.com>
2024-11-27 09:24:00 -0700

    Add dependencies

--- kotlin-multiplatform/composeApp/src/commonMain/kotlin/com/itkacher/raspberry/pico/thermo/App.kt
+++ kotlin-multiplatform/composeApp/src/commonMain/kotlin/com/itkacher/raspberry/pico/thermo/App.kt
@@ -1,31 +1,13 @@
 package com.itkacher.raspberry.pico.thermo
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import pico_thermo_sensor.composeapp.generated.resources.Res
-import pico_thermo_sensor.composeapp.generated.resources.compose_multiplatform
-import kotlin.text.set
 
 @Composable
 @Preview
