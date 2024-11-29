1c7d35bc91dfccd64eb1db65188cb924f094b68d
Eugene Tkachenko <gektor650@gmail.com>
2024-11-28 19:53:25 -0700

    Fix threading issues

--- kotlin-multiplatform/composeApp/src/commonMain/kotlin/com/itkacher/raspberry/pico/thermo/UdpServer.kt
+++ kotlin-multiplatform/composeApp/src/commonMain/kotlin/com/itkacher/raspberry/pico/thermo/UdpServer.kt
@@ -1,40 +1,51 @@
 package com.itkacher.raspberry.pico.thermo
 
-import io.ktor.network.selector.*
-import io.ktor.network.sockets.*
-import io.ktor.utils.io.*
+import io.ktor.network.selector.SelectorManager
+import io.ktor.network.sockets.BoundDatagramSocket
+import io.ktor.network.sockets.InetSocketAddress
+import io.ktor.network.sockets.aSocket
+import io.ktor.utils.io.readText
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.serialization.json.Json
-import kotlin.concurrent.Volatile
-import kotlin.coroutines.suspendCoroutine
 
 object UdpServer {
-    @Volatile
     private var serverSocket: BoundDatagramSocket? = null
     private val temperatureData = MutableSharedFlow<TemperatureData>()
+    private val mutex = Mutex()
 
     fun listenUdpMessages(scope: CoroutineScope): Flow<TemperatureData> {
         scope.launch(Dispatchers.IO + CoroutineName("BackgroundCoroutine")) {
-            val selectorManager = SelectorManager(Dispatchers.IO)
-            serverSocket?.dispose()
-            val newSocket = aSocket(selectorManager)
-                .udp()
-                .bind(InetSocketAddress("0.0.0.0", 5468))
-            serverSocket = newSocket
-            println("UDP server is listening on ${serverSocket?.localAddress}")
-            while (isActive && newSocket == serverSocket) {
-                val datagram = serverSocket?.receive() ?: break
-                val message = datagram.packet.readText()
-                val temperature = Json.decodeFromString<TemperatureData>(message)
-                temperatureData.emit(temperature)
-                println("Received message: $message from ${datagram.address}")
+            mutex.withLock {
+                try {
+                    val selectorManager = SelectorManager(Dispatchers.IO)
+                    serverSocket?.dispose()
+                    val newSocket = aSocket(selectorManager)
+                        .udp()
+                        .bind(InetSocketAddress("0.0.0.0", 5468))
+                    serverSocket = newSocket
+                    println("UDP server is listening on ${serverSocket?.localAddress}")
+                    while (isActive) {
+                        val datagram = serverSocket?.receive() ?: break
+                        val message = datagram.packet.readText()
+                        val temperature = Json.decodeFromString<TemperatureData>(message)
+                        try {
+                            temperatureData.emit(temperature)
+                            println("Received message: $message from ${datagram.address}")
+                        } catch (e: Exception) {
+                            println("Error emitting temperature data: ${e.message}")
+                        }
+                    }
+                } catch (e: Exception) {
+                    println("Error during UDP listening: ${e.message}")
+                } finally {
+                    serverSocket?.dispose()
+                    serverSocket = null
+                    println("Socket disposed.")
+                }
             }
-            println("Cleared socket")
-            serverSocket?.dispose()
-            serverSocket = null
         }
         return temperatureData
     }
